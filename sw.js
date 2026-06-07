59d6aa8544adb55781ac17e48533d19b25fd2630
Zyanuro <antonio.cuesta@talent-network.org>
2026-06-07 15:05:56 +0200

    Fix paths for GitHub Pages: use relative URLs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'lista-compra-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'lista-compra-v3';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
